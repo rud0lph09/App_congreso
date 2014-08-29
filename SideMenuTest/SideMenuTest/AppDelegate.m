823c7144313f9d5b8052d1d30d097e8ad507f9cd
Rodolfo Castillo <rovidrio@comunidad.unam.mx>
2014-08-29 16:13:07 -0500

    Se arreglaron Errores
    El compilador tenia errores con el linker y banderas, así que se
    removió de Build Settings dichos directorios, por el momento la app es
    funcional pero solo funciona el menú, en la próxima iteración se
    agregara Sign in con google plus.

--- SideMenuTest/SideMenuTest/AppDelegate.m
+++ SideMenuTest/SideMenuTest/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "GPPURLHandler.h"
+
 
 @implementation AppDelegate
 
@@ -17,14 +17,7 @@
     return YES;
     
 }
-- (BOOL)application: (UIApplication *)application
-            openURL: (NSURL *)url
-  sourceApplication: (NSString *)sourceApplication
-         annotation: (id)annotation {
-    return [GPPURLHandler handleURL:url
-                  sourceApplication:sourceApplication
-                         annotation:annotation];
-}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
